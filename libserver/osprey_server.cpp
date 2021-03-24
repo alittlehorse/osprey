57479813c873b7b6c53a380f15ab3f50b447a461
alittlehorse <my4993885@163.com>
2021-03-23 19:45:46 -0700

    add contract adapter.

--- libserver/osprey_server.cpp
+++ libserver/osprey_server.cpp
@@ -22,8 +22,14 @@ bool server_provider::on_generate_and_serialize_proof() {
     return true;
 }
 
-void server_provider::on_ready(const std::string &&config) {
+void server_provider::on_ready(const std::string &&config,
+                               const std::string& smart_contract_address) {
     _vp = new libserver::proof_params_config(std::move(config));
+    this->smart_contract_address = smart_contract_address;
+}
+
+const std::string &server_provider::get_address() {
+    return smart_contract_address;
 }
 
 bool server_requester::on_generate_key_pair() {
@@ -35,9 +41,11 @@ bool server_requester::on_generate_key_pair() {
     return true;
 }
 
-void server_requester::on_ready(const std::string &&config) {
+void server_requester::on_ready(const std::string &&config,
+                                const std::string& smart_contract_address){
     _vp = new libserver::proof_params_config(std::move(config));
     s = new libserver::groth16_server(*_vp);
+    this->smart_contract_address = smart_contract_address;
 }
 
 bool server_requester::on_verify() {
@@ -45,17 +53,59 @@ bool server_requester::on_verify() {
     assert(proof.has_value());
     auto vk = s->get_verification_key_from_file(_vp->get_verification_key_path());
     assert(vk.has_value());
-    return s->verify(vk.value(),proof.value());
+    if(s->verify(vk.value(),proof.value())){
+        if(osprey_plateform::on_finish_and_pay())
+            return true;
+    }
+    return false;
 }
+
+bool server_requester::on_lock_pay(double money) {
+    if(osprey_plateform::on_lock(money))return true;
+    else return false;
+}
+
+
+const std::string& server_requester::get_address() {
+    return smart_contract_address;
+}
+
+const std::string &osprey_plateform::get_address() {
+    return plateform_address;
+}
+
+void osprey_plateform::submit_requester_address(const std::string & requester_address) {
+    this->requester_address = requester_address;
+}
+
+void osprey_plateform::submit_server_address(const std::string &server_address) {
+    this->server_address = server_address;
+}
+
+bool osprey_plateform::on_lock(double lock_money) {
+    lock_money = lock_money;
+    //TODO:smart_contract:translate mony from requester to plateform address
+}
+
+bool osprey_plateform::on_finish_and_pay() {
+    //TODO: smart contract: translate mony from plateform address to server address
+}
+
+
 int main(){
-    server_provider sp{};
-    server_requester sq{};
+    server_provider sp;
+    server_requester sq;
+    osprey_plateform op;
 //simulate sq geneate keypair;
     sq.on_ready("avarage");
     assert(sq.on_generate_key_pair());
+    op.submit_requester_address(sq.get_address());
+    sq.on_lock_pay(0.3);
     //
     sp.on_ready("avarage");
     sp.on_generate_and_serialize_proof();
+    //
+    op.submit_server_address(sp.get_address());
     //
     assert(sq.on_verify()==true);
     printf("OK!");
--- libserver/osprey_server.hpp
+++ libserver/osprey_server.hpp
@@ -18,11 +18,13 @@ osprey server:
 
 class server_provider {
 public:
-    void on_ready(const std::string&& config);
+    void on_ready(const std::string&& config,const std::string& smart_contract_address);
     bool on_generate_and_serialize_proof();
+    const std::string& get_address();
 private:
     libserver::proof_params_config* _vp;
     libserver::groth16_server* s;
+    std::string smart_contract_address;
 };
 
 
@@ -31,11 +33,29 @@ public:
     bool on_complier_tinyram();
     bool on_generate_key_pair();
     bool on_verify();
-    void on_ready(const std::string&& config);
+    bool on_lock_pay(double money);
+    void on_ready(const std::string&& config,const std::string& smart_contract_address);
+    const std::string& get_address();
 
 private:
     libserver::proof_params_config* _vp;
     libserver::groth16_server* s;
+    std::string smart_contract_address;
+};
+
+class osprey_plateform{
+private:
+    //TODO: embedded plateform_address;
+    static std::string plateform_address;
+    std::string server_address;
+    std::string requester_address;
+    double lock_money;
+public:
+    static const std::string& get_address();
+    void submit_requester_address(const std::string& requester_address);
+    void submit_server_address(const std::string&);
+    static bool on_lock(double lock_money);
+    static bool on_finish_and_pay();
 };
 
 
