da70804c25eec1df65647f3c04af10eeb66763af
syheliel <45957390+syheliel@users.noreply.github.com>
2021-05-14 13:20:49 +0800

    Update EchoWebsock.h

--- application/EchoWebsock.h
+++ application/EchoWebsock.h
@@ -17,6 +17,6 @@ class EchoWebsock : public drogon::WebSocketController<EchoWebsock> {
   virtual void handleConnectionClosed(const WebSocketConnectionPtr &) override;
   WS_PATH_LIST_BEGIN
     //list path definitions here;
-    WS_PATH_ADD("/c");
+    WS_PATH_ADD("/compute_query");
   WS_PATH_LIST_END
 };
